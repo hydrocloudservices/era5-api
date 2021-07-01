5a4b428212a605a91eff8d1250ada8bb51256373
sebastienlanglois <sebastien.langlois@polymtl.ca>
2021-06-30 21:54:31 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/salut")
+async def getCity():
+    return {
+        "message": "Hey, salut!"
+    }
+
+if __name__ == "__main__" and os.environ.get("ENVIRONMENT") != "PRODUCTION":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

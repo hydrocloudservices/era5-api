59df4e102be4bc766fbc946b79b6ff40ba556093
sebastienlanglois <sebastien.langlois@polymtl.ca>
2021-06-30 21:58:53 -0400

    First commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/salut")
 async def getCity():
     return {
-        "message": "Hey, salut!"
+        "message": "Hey, salut, test!"
     }
 
 if __name__ == "__main__" and os.environ.get("ENVIRONMENT") != "PRODUCTION":
